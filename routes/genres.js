686296dc27b2b651c9fd1ff527abb9fe7d8134a1
GwonHyeok <me@ghyeok.io>
2018-10-29 23:38:44 +0900

    장르 썸네일 설정 기능 추가

--- routes/genres.js
+++ routes/genres.js
@@ -3,9 +3,30 @@ const router = express.Router();
 const { Genre } = require('../models');
 const asyncHandler = require('../helpers/asyncHandler');
 
+// Uploader
+const aws = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const upload = multer({
+  storage: multerS3({
+    s3: new aws.S3({}),
+    bucket: 'storage.youtube-player.com',
+    metadata: function(req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function(req, file, cb) {
+      cb(null, Date.now().toString())
+    }
+  })
+});
+
+
 // 장르 생성
-router.post('/', asyncHandler(async function(req, res) {
-  res.json({ data: {} })
+router.post('/', upload.single('thumbnail'), asyncHandler(async function(req, res) {
+  if (req.file) req.body.thumbnailUri = req.file.location;
+
+  const genre = await Genre.create(req.body);
+  res.json({ data: genre })
 }));
 
 // 장르 리스트
@@ -17,6 +38,14 @@ router.get('/', asyncHandler(async function(req, res) {
 // 장르 상세보기
 router.get('/:id', asyncHandler(async function(req, res) {
   const genre = await Genre.findOne({ where: { id: req.params.id } });
+  res.json({ data: genre })
+}));
+
+// 장르 수정
+router.put('/:id', upload.single('thumbnail'), asyncHandler(async function(req, res) {
+  if (req.file) req.body.thumbnailUri = req.file.location;
+
+  const genre = await Genre.update(req.body);
   res.json({ data: genre })
 }));
 

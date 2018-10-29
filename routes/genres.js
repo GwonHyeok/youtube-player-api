99222a48a8da7d1c894bada08719d37aef031541
GwonHyeok <me@ghyeok.io>
2018-10-29 23:51:16 +0900

    장르 업데이트 방식 수정

--- routes/genres.js
+++ routes/genres.js
@@ -46,7 +46,8 @@ router.get('/:id', asyncHandler(async function(req, res) {
 router.put('/:id', upload.single('thumbnail'), asyncHandler(async function(req, res) {
   if (req.file) req.body.thumbnailUri = req.file.location;
 
-  const genre = await Genre.update({ where: { id: req.params.id } }, req.body);
+  const genre = await Genre.findOne({ where: { id: req.params.id } });
+  await genre.update(req.body);
   res.json({ data: genre })
 }));
 
